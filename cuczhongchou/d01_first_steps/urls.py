a39db74c0cc2f36565d5f665c29e773e97cccd8f
zhchen@MacAir <zhchen@cuc.edu.cn>
2015-07-28 09:59:05 +0800

    完成 tutorial04
    支持 通用  view

--- cuczhongchou/d01_first_steps/urls.py
+++ cuczhongchou/d01_first_steps/urls.py
@@ -9,12 +9,21 @@ import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
-     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/
+    # 手工实现view 
+    #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    #url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
+    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+
+    # file:///Users/chen/coding/documentations/django-docs-1.8-en/intro/tutorial04.html#amend-urlconf
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 
 
     url(r'^articles/([0-9]{4})/$', views.year_archive),
--- cuczhongchou/d01_first_steps/views.py
+++ cuczhongchou/d01_first_steps/views.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.http.request import HttpRequest
-from django.http.response import HttpResponse, Http404
+from django.http.response import HttpResponse, Http404, HttpResponseRedirect
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
 from django.template import RequestContext, loader
-
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-from .models import Article, Question
+from .models import Article, Question, Choice
 
 
 """
     Tutorial 03 file:///Users/chen/coding/documentations/django-docs-1.8-en/intro/tutorial03.html
 """
 
+
+"""
+    版本1: 手工实现
+"""
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # output = ', '.join([p.question_text for p in latest_question_list])
     # #return HttpResponse(output)
-    template = loader.get_template('d01_first_steps/index.html')
+    template = loader.get_template('d01_first_steps/polls_index.html')
     context = RequestContext(request, {
         'latest_question_list': latest_question_list,
     })
@@ -32,16 +36,63 @@ def detail(request, question_id):
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
     #     raise Http404(u"找不到这个投票问题")
+
+    #上面代码的简化版本
     question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, 'd01_first_steps/detail.html', {'question': question})
+    return render(request, 'd01_first_steps/polls_detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    #简单模拟实现
+    #response = "You're looking at the results of question %s."
+    #return HttpResponse(response % question_id)
+
+    #DAO操作
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'd01_first_steps/polls_results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    #简单实现了此函数
+    #return HttpResponse("You're voting on question %s." % question_id)
+
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):  #处理两个异常 KeyError 没有post choice, NotExist 找不到当前投票
+        # Redisplay the question voting form.
+        return render(request, 'd01_first_steps/polls_detail.html', {
+            'question': p,
+            'error_message': "请选择一个选项 ",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+"""
+版本2 : 使用通用模板 file:///Users/chen/coding/documentations/django-docs-1.8-en/intro/tutorial04.html#amend-views
+"""
+
+class IndexView(generic.ListView):
+    template_name = 'd01_first_steps/polls_index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'd01_first_steps/polls_detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'd01_first_steps/polls_results.html'
 
 
 """
