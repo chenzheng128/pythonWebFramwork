0baa91f8f10773e4659aa80229ac1247a2d9608b
zhchen@MacAir <zhchen@cuc.edu.cn>
2015-07-28 17:17:36 +0800

    调整学堂教程url

--- cuczhongchou/cuczhongchou/urls.py
+++ cuczhongchou/cuczhongchou/urls.py
@@ -34,10 +34,10 @@ urlpatterns = [
     url(r'^step02/$', 'learn.views.step02', name='step02'),# Notice this line
     
     # step03 Examples: 从/add/?a=4&b=5 读取参数
-    url(r'^add/$', 'learn.views.add', name='add'), # 注意修改了这一行
+    url(r'^step03a/$', 'learn.views.add', name='add'), # 注意修改了这一行
 
     # 采用 /add/3/4/ 这样的网址的方式
-    url(r'^add/(\d+)/(\d+)/$', 'learn.views.add2', name='add2'),
+    url(r'^step03b/(\d+)/(\d+)/$', 'learn.views.add2', name='add2'),
 
     # step04 Examples: 增加learn 应用中的 views.py 模块 中的 index(request) 方法
     url(r'^step04a/$', 'learn.views.step04a', name='learnStep04a'),# Notice this line
